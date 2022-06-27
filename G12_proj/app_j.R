646512d8f69b9323748960671d69c5e91cddf00a
Jeremiahlam <jeremiahlam.2021@smu.edu.sg>
2022-06-28 02:37:12 +0800

    1st commit from jeremiah for app

---
+++ G12_proj/app_j.R
@@ -0,0 +1,315 @@
+packages <- c('shiny', 'shinydashboard', 'shinythemes','tsibble','tseries',
+              'plotly', 'tidyverse', 'ggstatsplot','zoo','forecast',
+              'tools','shinyWidgets','readxl','bslib','dplyr','lubridate','patchwork')
+
+for (p in packages){
+  library(p, character.only=T)
+}
+
+jobs <- read_rds("data/rds/jobs.rds")
+exam <- read_csv("data/Exam_data.csv")
+travel_filt <- readRDS('data/rds/travel_filt.rds')
+restaurants <- read_csv("data/Restaurants.csv")
+pubs <- read_csv("data/Pubs.csv")
+
+
+data_travel= travel_filt %>%
+  mutate(weekday = weekdays(checkInTime),
+         day = day(checkInTime),
+         month=as.character(checkInTime,"%b %y"),
+         year = year(checkInTime),
+         monthYear = as.yearmon(checkInTime),
+         travelEndLocationId=as.character(travelEndLocationId),
+         timeSpent = checkOutTime - checkInTime,
+         participantId=as.character(participantId),
+         purpose=as.character(purpose))
+data_travel$timeSpent <- as.numeric(as.character(data_travel$timeSpent))
+
+data_travel <- data_travel[,c("participantId","travelStartLocationId", "travelEndLocationId", "purpose",  "amountSpent","timeSpent","weekday","day","month","year","monthYear")]
+
+group_pub <-merge(x=data_travel, y=pubs, by.x = "travelEndLocationId", by.y = "pubId")
+group_pub$venue <- "Pub"
+group_pub$foodCost <- 0.00
+
+group_restaurant<-merge(x=data_travel, y=restaurants, by.x = 'travelEndLocationId', by.y =  'restaurantId')
+group_restaurant$venue <- "Restaurant"
+group_restaurant$hourlyCost <- 0.00
+
+group <-rbind(group_pub, group_restaurant)
+
+period <- unique(na.omit(group$monthYear))
+
+
+ui <- fluidPage(
+  theme = bs_theme(bootswatch = "minty"),
+  navbarPage(
+  title = "VAST Challenge 3 - €conomic",
+  fluid = TRUE,
+  collapsible = TRUE,
+  theme=shinytheme("united"),
+  id = "navbarID",
+  tabPanel(title = "Overview",icon = icon("globe"),
+           mainPanel(
+             ### the rest of your code
+           )),
+  navbarMenu("Exploratory", icon =icon("list-alt"),
+             tabPanel("Revenue",
+                      sidebarPanel(
+                        width = 3,
+                        radioButtons("venue", 
+                                     label = "Select the Venue:",
+                                     choices = list("Pub" = "Pub", 
+                                                    "Restaurant" = "Restaurant"), 
+                                     selected = "Pub"
+                                     ),
+                        
+                        selectInput(input = "period", 
+                                    label = "Choose Period:",
+                                    choices = c(period),
+                                    multiple = TRUE,
+                                    selected = period[1]),
+
+                        radioButtons("group", 
+                                     label = "Select X-axis Interval:",
+                                     choices = list("daily" = "day", 
+                                                    "weekly" = "weekday",
+                                                    "monthly" = "month"), 
+                                     selected = "weekly")
+                                    ),
+                        
+                      mainPanel(
+                        width = 9,
+                        fluidRow(
+                          column(width = 9,
+                                 wellPanel("Test Graph",
+                                           plotOutput("revenue",
+                                                      height = "500px")
+                                 )
+                          )
+                        )
+                      )
+                      
+
+             )
+             ),
+  navbarMenu("Analysis",icon =icon("cog", lib = "glyphicon"),
+             tabPanel("Between group"),
+             tabPanel("Within group"),
+             tabPanel("ANOVA",
+                      sidebarLayout(
+                        sidebarPanel(width = 3,
+                                     selectInput(inputId = "xvariable",
+                                                 label = "Select x-variable:",
+                                                 choices = c("Class" = "CLASS",
+                                                             "Gender" = "GENDER",
+                                                             "Race" = "RACE"),
+                                                 selected = "GENDER"),
+                                     selectInput(inputId = "yvariable",
+                                                 label = "Select y-variable:",
+                                                 choices = c("English" = "ENGLISH",
+                                                              "Maths" = "MATHS",
+                                                              "Science" = "SCIENCE"),
+                                                 selected = "ENGLISH"),
+                                     selectInput(inputId = "test",
+                                                 label = "Type of statistical test:",
+                                                 choices = c("parametric" = "p",
+                                                             "nonparametric" = "np",
+                                                             "robust" = "r",
+                                                             "Bayes Factor" = "bf"),
+                                                 selected = "p"),
+                                     selectInput(inputId = "plotType",
+                                                 label = "Type of plot:",
+                                                 choices = c("boxviolin" = "boxviolin",
+                                                             "box" = "box",
+                                                             "violin" = "violin"),
+                                                 selected = "boxviolin"),
+                                     textInput(inputId = "plotTitle",
+                                               label = "Plot title",
+                                               placeholder = "Enter text to be used as plot title"),
+                                     actionButton(inputId = "goButton", 
+                                                  "Go!")
+                        ),
+                        mainPanel(width = 9,
+                                  box(
+                                    plotOutput("boxplot",
+                                               height = "500px"))
+                        )
+                      )),
+             tabPanel("Correlation",
+                      sidebarLayout(
+                        sidebarPanel(width = 3,
+                                     selectInput(inputId = "xvariable1",
+                                                 label = "Select x-variable:",
+                                                 choices = c("English" = "ENGLISH",
+                                                             "Maths" = "MATHS",
+                                                             "Science" = "SCIENCE"),
+                                                 selected = "ENGLISH"),
+                                     selectInput(inputId = "yvariable1",
+                                                 label = "Select y-variable:",
+                                                 choices = c("English" = "ENGLISH",
+                                                             "Maths" = "MATHS",
+                                                             "Science" = "SCIENCE"),
+                                                 selected = "MATHS"),
+                                     selectInput(inputId = "test1",
+                                                 label = "Type of statistical test:",
+                                                 choices = c("parametric" = "p",
+                                                             "nonparametric" = "np",
+                                                             "robust" = "r",
+                                                             "Bayes Factor" = "bf"),
+                                                 selected = "p"),
+                                     checkboxInput(inputId = "marginal", 
+                                                   label = "Display marginal graphs", 
+                                                   value = TRUE),
+                                     textInput(inputId = "plotTitle1",
+                                               label = "Plot title",
+                                               placeholder = "Enter text to be used as plot title"),
+                                     actionButton(inputId = "goButton1", 
+                                                  "Go!")
+                        ),
+                        mainPanel(width = 9,
+                                  box(
+                                    plotOutput("corrPlot",
+                                               height = "500px"))
+                        )
+                      ))
+  ),
+  navbarMenu("Employer",icon = icon("bar-chart-o"),
+             tabPanel("Employer Health",
+                      sidebarPanel(width = 2.5,
+                                   selectInput(inputId = "educationrequired",
+                                               label = "Select Education Requirement",
+                                               choices = c("Low" = "Low",
+                                                           "High School or College" = "HighSchoolOrCollege",
+                                                           "Bachelors" = "Bachelors",
+                                                           "Graduate" = "Graduate",
+                                                           "All" = "All"),
+                                               selected = "All"),
+                                   sliderInput(inputId = "bins",
+                                               label = "Number of Bins",
+                                               min = 4,
+                                               max = 12,
+                                               value = 6)
+                                   ),
+                      mainPanel(width = 10, plotOutput("Wagespread"))),
+             tabPanel("Employment Patterns"),
+             tabPanel("Turnover"))
+  )
+)
+
+
+#========================#
+###### Shiny Server ######
+#========================#
+
+server <- function(input, output){
+  
+##### Shiny Server: Between Group Analysis ##### 
+  output$boxplot <- renderPlot({
+    input$goButton
+    set.seed(1234)
+    
+    ggbetweenstats(
+      data = exam,
+      x = !!input$xvariable, 
+      y = !!input$yvariable,
+      type = input$test,
+      title = isolate({
+        toTitleCase(input$plotTitle)
+      }),
+      plot.type = input$plotType,
+      mean.ci = TRUE, 
+      pairwise.comparisons = TRUE, 
+      pairwise.display = "s",
+      p.adjust.method = "fdr",
+      messages = FALSE)
+  })
+  
+  output$revenue <- renderPlot({
+    print(input$venue)
+    print(input$period)
+    print(input$group)
+    p <- group %>%
+      filter(monthYear == input$period, venue == input$venue ) %>%
+      group_by(!!!rlang::syms(input$group), travelEndLocationId) %>%
+      summarise(amountSpent = (sum(amountSpent))) %>%
+      ggplot(aes_string(x=input$group, y="amountSpent", group="travelEndLocationId")) +
+      geom_line(aes(color=travelEndLocationId),show.legend = TRUE)+
+      labs(
+        y= 'Revenue (Thousands$)',
+        title = "Revenue Pubs - 2022",
+        caption = "Ohio USA"
+      ) 
+    p
+  })
+##### Shiny Server: Correlation Analysis #####   
+  output$corrPlot <- renderPlot({
+    input$goButton1
+    set.seed(1234)
+    
+  ggscatterstats(
+    data = exam,
+    x = !!input$xvariable1, 
+    y = !!input$yvariable1,
+    marginal = input$marginal,
+    title = isolate({
+      toTitleCase(input$plotTitle1)
+    }),
+    conf.level = 0.95,
+    bf.prior = 0.707)
+    })
+
+  ##### Shiny Server: Employer: Employer Health ##### 
+  
+  ##### Education Filter#####
+  jobs2 <- reactive({
+    if(input$educationrequired == "All") {
+      jobs2 <- jobs
+    } else if(input$educationrequired =="Low") {
+      jobs2 <- jobs %>%
+        filter(educationRequirement == "Low")
+    } else if(input$educationrequired =="HighSchoolOrCollege"){
+      jobs2 <- jobs %>%
+        filter(educationRequirement =="HighSchoolOrCollege")
+    } else if(input$educationrequired =="Bachelors"){
+      jobs2 <- jobs %>%
+        filter(educationRequirement =="Bachelors")
+    } else {
+      jobs2 <- jobs %>%
+        filter(educationRequirement =="Graduate")
+    }
+  })
+  jobs3 <- jobs[,-7]
+  
+  output$Wagespread <- renderPlot({
+    j <- ggplot(jobs, aes(x = hourlyRate,
+                       fill = educationRequirement)) +
+      geom_histogram(data = jobs3,
+                     fill = "dark grey",
+                     alpha = .5,
+                     bins = input$bins) +
+      geom_histogram(colour = "black", 
+                     bins = input$bins) +
+      facet_wrap(~ educationRequirement) + 
+      xlab("Hourly Rate") +
+      ylab("No. of Job Openings") +
+      labs(title = "Spread of Hourly Wages by Education Level\n vs. Overall Job Market") +
+      guides(fill = FALSE, scale = "none") +
+      theme_bw()
+    
+    j2 <- ggplot(jobs2(), aes(x = hourlyRate,
+                           fill = "dark blue")) +
+      geom_histogram(colour = "black", 
+                     bins = input$bins) +
+      xlab("Hourly Rate") +
+      ylab("No. of Job Openings") +
+      labs(title = paste("Spread of Hourly Wages for","\n",input$educationrequired, "education level")) +
+      guides(fill = FALSE, scale = "none") +
+      theme_bw()
+    
+    j+j2
+  })
+  
+  
+}
+
+shinyApp(ui = ui, server = server)
